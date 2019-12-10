2432892f472bf7cbfa6c87c0b4308c3ae127ac60
Sonwabile Nomnganga <Sonwabile.Nomnganga@StandardBank.co.za>
2019-12-10 13:23:08 +0200

    Filtering

--- src/main/java/za/co/absa/africanacity/billing/controller/AbsaBillingFileUploaderController.java
+++ src/main/java/za/co/absa/africanacity/billing/controller/AbsaBillingFileUploaderController.java
@@ -1,24 +1,16 @@
 package za.co.absa.africanacity.billing.controller;
 
 import com.github.ffpojo.exception.FFPojoException;
-import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import za.co.absa.africanacity.billing.service.BillingUploaderService;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @RestController
@@ -29,19 +21,24 @@ public class AbsaBillingFileUploaderController {
     public static final String FILE_READ_ERROR = "Error occurred while reading the file. ";
     public static final String FILE_PARSING_ERROR = "Error occurred while parsing the file. ";
     public static final String SUCCESS_UPLOAD = "Billing extract uploaded with success ";
+
     @Autowired
     private BillingUploaderService service;
 
     @RequestMapping(value = "/post", method= RequestMethod.POST, produces = "application/json")
     public Response file(@FormDataParam("file") InputStream inputStream){
 
-        log.info("Upload request received......");
+        log.info("New file upload request received..........");
 
         if (inputStream == null)
             return Response.status(400).entity(INVALID_INPUT_DATA).build();
 
         try {
-            saveStreamToDatabase(inputStream);
+            log.info(" to save");
+            saveStream(inputStream);
+            log.info("done saving");
+
+            inputStream.close();
         } catch (IOException e) {
             return Response.status(500).entity(FILE_READ_ERROR + e.getMessage()).build();
         } catch (FFPojoException e) {
@@ -52,10 +49,12 @@ public class AbsaBillingFileUploaderController {
                 .entity(SUCCESS_UPLOAD).build();
     }
 
-    private void saveStreamToDatabase(InputStream inputStream) throws IOException, FFPojoException {
+    private void saveStream(InputStream inputStream) throws IOException, FFPojoException {
 
+        log.info("to save stream");
         InputStreamReader isr = new InputStreamReader(inputStream);
         BufferedReader in = new BufferedReader(isr);
+
         String data;
         while ((data = in.readLine()) != null) {
             service.saveData(data);
--- src/main/java/za/co/absa/africanacity/billing/service/impl/BillingUploaderServiceImpl.java
+++ src/main/java/za/co/absa/africanacity/billing/service/impl/BillingUploaderServiceImpl.java
@@ -2,6 +2,7 @@ package za.co.absa.africanacity.billing.service.impl;
 
 import com.github.ffpojo.FFPojoHelper;
 import com.github.ffpojo.exception.FFPojoException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.co.absa.africanacity.billing.domain.BillingRecord;
@@ -11,6 +12,7 @@ import za.co.absa.africanacity.billing.service.BillingUploaderService;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class BillingUploaderServiceImpl implements BillingUploaderService {
 
@@ -20,6 +22,7 @@ public class BillingUploaderServiceImpl implements BillingUploaderService {
     @Override
     public void saveData(String data) throws FFPojoException {
 
+        log.info("to save to data base");
         if(data != null) {
             FFPojoHelper ffpojo = FFPojoHelper.getInstance();
             repository.saveAndFlush(ffpojo.createFromText(BillingRecord.class, data));
